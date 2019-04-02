393c2d5423081d2eec8fea715f90d1f7d6d09147
ishamSIE <ishamaaroufi@gmail.com>
2019-04-02 03:01:14 +0200

    Update NoteAdapter.kt

--- app/src/main/java/com/example/firebase_project_5/NoteAdapter.kt
+++ app/src/main/java/com/example/firebase_project_5/NoteAdapter.kt
@@ -8,12 +8,9 @@ import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.note_layout.view.*
 
 
-// hna creet constructor dial arrayadapter
-// + poi 3tetto context + arraylist  poi fi ArrayAdapter 3tetto nome dial noteclass fin andi les variabile fi constructor dialo 3tetto nass les infos
-// ra9m 0 zero 3tetolo bach manliltich ch7al min datti ghanktab di agenda  m// momkin na3teha 3 mais maykench nfoout 3 dial mawa3id fi agenda diali
 
 
- // context derto hna lta7t  bach nasta3mlo fi layoutinflater  guarda giu amoo :)))))))))
+
 
 class NoteAdapter(context:Context, noteList:ArrayList<noteClass>) : ArrayAdapter<noteClass>(context,0, noteList) {
 
@@ -22,7 +19,6 @@ class NoteAdapter(context:Context, noteList:ArrayList<noteClass>) : ArrayAdapter
         //return super.getView(position, convertView, parent)
 
 
-         // hna jebt layout LIGAHDI YBn fih les informations
 
 
 
